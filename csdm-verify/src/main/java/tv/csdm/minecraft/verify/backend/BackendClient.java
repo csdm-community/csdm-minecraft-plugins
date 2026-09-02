acc9deed4091581d31891672d787c7a9e74bc466
aguacatazo <noxinotnoxi@gmail.com>
2026-09-01 21:53:33 -0600

    feat: query verified identity on join

--- csdm-verify/src/main/java/tv/csdm/minecraft/verify/backend/BackendClient.java
+++ csdm-verify/src/main/java/tv/csdm/minecraft/verify/backend/BackendClient.java
@@ -1,10 +1,13 @@
 package tv.csdm.minecraft.verify.backend;
 
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import tv.csdm.minecraft.verify.model.IdentityStatusResponse;
 import tv.csdm.minecraft.verify.model.VerificationRequest;
 import tv.csdm.minecraft.verify.model.VerificationResponse;
 
 public interface BackendClient {
     CompletableFuture<VerificationResponse> verify(VerificationRequest request);
-}
 
+    CompletableFuture<IdentityStatusResponse> identityStatus(UUID minecraftUuid);
+}
